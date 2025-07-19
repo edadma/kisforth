f5871da49098dc5259556b73e31deeae31fbf98d
Edward A Maxedon <edadma@gmail.com>
2025-07-19 02:37:10 -0400

    clean up code

--- host/src/main.c
+++ host/src/main.c
@@ -7,13 +7,13 @@
 
 int main(int argc, char* argv[]) {
   forth_system_init();
-  print_startup_banner("Host Development");
 
   if (argc > 1 && strcmp(argv[1], "test") == 0) {
     word_t* test_word = find_word("TEST");
     printf("Running tests...\n\n");
     execute_word(test_word);
   } else {
+    print_startup_banner("Host Development");
     repl();
   }
 
