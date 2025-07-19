8833a5b5b9fd043465ec2f5f61af3ed43128cf41
Edward A Maxedon <edadma@gmail.com>
2025-07-19 02:38:00 -0400

    clean up code

--- pico/src/main.c
+++ pico/src/main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
-#include "startup.h"
 #include "repl.h"
+#include "startup.h"
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    while (!stdio_usb_connected()) {
-        sleep_ms(100);
-    }
+  while (!stdio_usb_connected()) {
+    sleep_ms(100);
+  }
 
-    sleep_ms(500);
-    printf("\n");
+  sleep_ms(500);
+  printf("\n");
 
-    forth_system_init();
-    print_startup_banner("Pico");
+  forth_system_init();
+  print_startup_banner("Pico");
 
-    repl();
-    return 0;
+  repl();
+  return 0;
 }
